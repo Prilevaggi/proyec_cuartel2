e0f6ae25d6c656903a9406910fda56b7c9dc8c20
Prilevaggi <103548288+Prilevaggi@users.noreply.github.com>
2023-12-14 18:24:45 -0300

    agrego base de datos utilizada

---
+++ basecuartel (1)/prueba_jerarquias.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: prueba
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `jerarquias`
+--
+
+LOCK TABLES `jerarquias` WRITE;
+/*!40000 ALTER TABLE `jerarquias` DISABLE KEYS */;
+INSERT INTO `jerarquias` VALUES (1,'Bombero'),(2,'Cabo'),(3,'Cabo Primero'),(4,'Sargento'),(5,'Sargento Primero'),(6,'Suboficial Principal'),(7,'Suboficial Mayor'),(8,'Oficial Ayudante'),(9,'Oficial Inspector'),(10,'Oficial Principal'),(11,'Sub-Comandante'),(12,'comandante'),(13,'Comandante Mayor'),(15,'Comandante General');
+/*!40000 ALTER TABLE `jerarquias` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-14 18:23:38
